e725d745ecab3a7e116d562fdec767cd302f7477
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-02 12:22:13 -0600

    solving newRecipe function

--- src/main/java/guru/springframework/controllers/IngredientController.java
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -52,7 +52,8 @@ public class IngredientController {
     public String newRecipe(@PathVariable String recipeId, Model model){
 
         //make sure we have a good id value
-        RecipeCommand recipeCommand = recipeService.findCommandById(recipeId).block();
+        recipeService.findCommandById(recipeId);
+
         //todo raise exception if null
 
         //need to return back parent id for hidden form property
