dc7a527065a06ebc5b1fffa178f17bb0dd985dc0
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-02 16:17:46 -0600

    refactoring exceptionHandler

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,18 +14,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    /*@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
+    public String handleNumberFormat(Exception exception, Model model){
 
         log.error("Handling Number Format Exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }*/
+        return "400error";
+    }
 }
